ffd8c62e7ab37117b38424711572ffb5bca9b1fe
meeteedee <meepoohfisho@gmail.com>
2026-03-27 16:48:21 +0700

    Coroutine

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,6 +11,8 @@ public class PlayerController : MonoBehaviour
     private InputAction moveAction;
     private InputAction smashAction;
     private InputAction breakAction;
+    
+    public bool hasPowerUp = false;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -31,5 +33,40 @@ public class PlayerController : MonoBehaviour
         {
             rb.linearVelocity = Vector3.zero;
         }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            if (hasPowerUp == true)
+            {
+                var rb = collision.gameObject.GetComponent<Rigidbody>();
+                var dir = collision.transform.position - transform.position;
+                rb.AddForce(100 * dir.normalized, ForceMode.Impulse);
+            }
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUp"))
+        {
+            hasPowerUp = true;
+            Destroy(other.gameObject);
+            if (countdonwRoutine != null)
+            {
+                StopCoroutine(countdonwRoutine);
+            }
+            countdonwRoutine = StartCoroutine(PowerUpCountdown());
+        }
+    }
+
+    private Coroutine countdonwRoutine;
+    IEnumerator PowerUpCountdown()
+    {
+        yield return new WaitForSeconds(10f);
+        hasPowerUp = false;
     }
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,19 +1,34 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class SpawnManager : MonoBehaviour
 {
     public Transform[] spawnPoints;
     public GameObject enemyPrefab;
-
+    public Rigidbody box;
+    public Coroutine goodbyeRoutine;
     void Start()
     {
+        StartCoroutine(SpawnRoutine());
         //InvokeRepeating(nameof(RandomSpawn), 0, 5);
         //StartCoroutine(Hello());
-        StartCoroutine(Goodbye());
+        //StartCoroutine(Goodbye());
+        //StartCoroutine(MoveBox());
+        //goodbyeRoutine = StartCoroutine(Goodbye());
 
     }
 
+    IEnumerator SpawnRoutine()
+    {
+        yield return new WaitForSeconds(5);
+        while (true)
+        {
+            RandomSpawn();
+            yield return new WaitForSeconds(3);
+        }
+        
+    }
     void RandomSpawn()
     {
         var index = Random.Range(0, spawnPoints.Length);
@@ -21,19 +36,58 @@ public class SpawnManager : MonoBehaviour
         Instantiate(enemyPrefab, spawnPoint.position, Quaternion.identity);
     }
 
+    private void Update()
+    {
+        /*if (Time.time > 5)
+            if (goodbyeRoutine != null)
+            {
+                StartCoroutine(goodbyeRoutine);
+            }*/
+    }
+
+    /*IEnumerator MoveBox()
+    {
+        while (true)
+        {
+        box.linearVelocity = 10 * Vector3.up;
+        yield return new WaitForSeconds(3);
+        box.linearVelocity = 10 * Vector3.right;
+        yield return new WaitForSeconds(3);
+        box.linearVelocity = 10 * Vector3.down;
+        yield return new WaitForSeconds(3);
+        box.linearVelocity = 10 * Vector3.left;
+        }
+    }
     IEnumerator Goodbye()
     {
         while (true)
         {
             Debug.Log("Bye" + Time.frameCount + " " + Time.time);
             //yield return new WaitForSeconds(1);
-            yield return null;
+            yield return null();
+            //if (Time.time > 5)
+           // {
+          //      yield break;
+          //  }
+            yield return Hello();
+            
         }
         
     }
     IEnumerator Hello()
     {
         Debug.Log("Hello" + Time.frameCount);
+        Debug.Log("Hello" + Time.frameCount);
+        Debug.Log("Hello" + Time.frameCount);
         yield return null;
-    }
+        Debug.Log("Hello" + Time.frameCount);
+        yield return null;
+        Debug.Log("Hello" + Time.frameCount);
+        yield return null;
+        yield return null;
+        yield return null;
+        yield return null;
+        yield return null;
+        Debug.Log("Hello" + Time.frameCount);
+    }*/
 }
